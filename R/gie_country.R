5f0dbf2db24f6f5c688d7bd806e421cbf8264022
Kenneth Rose <kennethrose82@gmail.com>
2019-07-31 14:04:31 +0200

    New function to get the country data.

---
+++ R/gie_country.R
@@ -0,0 +1,56 @@
+#' Get the aggregated historical data export for for a specific country.
+#'
+#' @param country_code Two digit country code. Ex: NL, DE, DK, SE, FI etc.
+#' @param api_key The default is NULL and searches for your GIE_PAT in you .Renviron
+#'     file.
+#'
+#' @export
+#'
+#' @examples {
+#'
+#' library(gie)
+#'
+#' de <- gie_country("DE")
+#' nl <- gie_country("NL")
+#'
+#' }
+#'
+gie_country <- function(country_code, api_key = NULL){
+
+  area <- toupper(country_code)
+
+  if(length(country_code) > 1){
+    stop("country_code only accepts a vector of length one.")
+  }
+
+  if(is.null(api_key)){
+    api_key <- Sys.getenv("GIE_PAT")
+  }
+
+  url <- paste0("https://agsi.gie.eu/api/data/", country_code)
+
+  resp <- httr::GET(url = url,
+                    httr::add_headers("x-key" = api_key))
+
+  if(httr::status_code(resp) != 200){
+    status_httr <- httr::http_status(resp)
+    stop(paste("Category:", status_httr$category,
+               "Reason:", status_httr$reason,
+               "Message:", status_httr$message))
+  }
+
+  cont <- httr::content(resp, as = "text", encoding = "UTF-8")
+
+  cont_df <- jsonlite::fromJSON(cont)
+  cont_df$info <- sapply(cont_df$info, function(x){
+    if(length(x) < 1){
+      x <- as.character(NA)
+    } else {
+      x <- paste(x, collapse = ";")
+    }
+    x
+  })
+  cont_df <- suppressMessages(readr::type_convert(cont_df))
+
+  cont_df
+}
