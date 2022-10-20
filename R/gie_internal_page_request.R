c7424a9041a89222db364eeaaf25e80b3b030a79
Markus ›fin‹ Hametner <fin+github@xbhd.org>
2022-10-20 19:23:41 +0200

    fix request size in gie query

--- R/gie_internal_page_request.R
+++ R/gie_internal_page_request.R
@@ -14,7 +14,7 @@ gie_internal_page_request <- function(url, api_key, max_pages=10, country_code) 
 
 
   resp <- httr::RETRY('GET',
-                      url = url,
+                      url = paste0(url, '?size=300',
                       httr::add_headers("x-key" = api_key))
 
   if(httr::status_code(resp) != 200){
@@ -44,7 +44,7 @@ gie_internal_page_request <- function(url, api_key, max_pages=10, country_code) 
   while(page<cont_obj$last_page && page<max_pages) {
     page <- page + 1
 
-    url <- paste0("https://agsi.gie.eu/api/data/", country_code, "?page=", page)
+    url <- paste0("https://agsi.gie.eu/api/data/", country_code, "?size=300&page=", page)
 
     resp <- httr::RETRY('GET',
                         url = url,
