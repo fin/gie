ce153b1f5da007ff86f8c477abae1b65816e062f
Kenneth Rose <kennethrose82@gmail.com>
2022-10-25 22:32:46 +0200

    Add functions to get company data and facility data.

--- R/gie_company_eic.R
+++ R/gie_company_eic.R
@@ -1,16 +1,21 @@
-#' Get the historical data export for for a specific facility from a company within a country.
+#' Get the historical data export for for a specific company within a country.
 #'
 #' @param country_code Two digit country code. Ex: NL, DE, DK, SE, FI etc.
-#' @param eic_code The 21 digit eic code of the facility as found on the API page.
-#' @param eic_company_code The 21 digit eic code of the company as found on the API page.
+#' @param company_code The 21 digit eic code of the company as found on the API page.
 #' @param api_key The default is NULL and searches for your GIE_PAT in you .Renviron
 #'     file.
-#' @param ... Forwarded to [gie_internal_page_request()]
+#' @param max_pages Maximum pages. Defaults to 5000 to get all pages.
 #'
 #' @export
 #'
+#' @examples
 #'
-gie_gas_company_eic <- function(country_code, eic_code, eic_company_code, api_key = NULL, ...){
+#' library(gie)
+#'
+#' tail(gie_gas_company_eic(country_code = "DE", company_code = "21X000000001160J"))
+#'
+#'
+gie_gas_company_eic <- function(country_code, company_code, api_key = NULL, max_pages = 5000){
 
   area <- toupper(country_code)
 
@@ -19,9 +24,10 @@ gie_gas_company_eic <- function(country_code, eic_code, eic_company_code, api_ke
   }
 
 
-  url <- paste0("https://agsi.gie.eu/api/data/", eic_code, "/", country_code, "/", eic_company_code)
+  cont_df <- gie_pagination_api(hostname = "agsi.gie.eu", country = country_code,
+                            company = company_code,
+                            max_pages = max_pages, api_key = api_key)
 
-  cont_df <- gie_internal_page_request(url, api_key, ...)
 
   if(nrow(cont_df) == 0){
     stop("No data with these parameters.")
@@ -36,32 +42,30 @@ gie_gas_company_eic <- function(country_code, eic_code, eic_company_code, api_ke
     x
   })
   cont_df <- suppressMessages(readr::type_convert(cont_df, na = c("", "NA", "-")))
+  cont_df <- dplyr::arrange(cont_df, gasDayStart)
 
   cont_df
 }
 
-
-#' Get the historical data export for for a specific facility from a company within a country.
+#' Get the historical data export for for a specific company within a country.
 #'
 #' @param country_code Two digit country code. Ex: NL, DE, DK, SE, FI etc.
-#' @param eic_code The 21 digit eic code of the facility as found on the API page.
-#' @param eic_company_code The 21 digit eic code of the company as found on the API page.
+#' @param company_code The 21 digit eic code of the company as found on the API page.
 #' @param api_key The default is NULL and searches for your GIE_PAT in you .Renviron
 #'     file.
+#' @param max_pages Maximum pages. Defaults to 5000 to get all pages.
 #'
 #' @export
 #'
-#' @examples {
+#' @examples
 #'
 #' library(gie)
 #'
 #' eic <- gie_lng_company_eic(country_code = "DE",
-#'                            eic_code = "21W000000000100J",
-#'                            eic_company_code = "21X000000001368W")
+#'                            company_code = "21X000000001368W")
 #'
-#' }
 #'
-gie_lng_company_eic <- function(country_code, eic_code, eic_company_code, api_key = NULL){
+gie_lng_company_eic <- function(country_code, company_code, api_key = NULL, max_pages = 5000){
 
   area <- toupper(country_code)
 
@@ -69,27 +73,13 @@ gie_lng_company_eic <- function(country_code, eic_code, eic_company_code, api_ke
     stop("country_code only accepts a vector of length one.")
   }
 
-  if(is.null(api_key)){
-    api_key <- Sys.getenv("GIE_PAT")
-  }
 
-  url <- paste0("https://alsi.gie.eu/api/data/", eic_code, "/", country_code, "/", eic_company_code)
+  cont_df <- gie_pagination_api(hostname = "alsi.gie.eu", country = country_code,
+                                company = company_code,
+                                max_pages = max_pages, api_key = api_key)
 
-  resp <- httr::GET(url = url,
-                    httr::add_headers("x-key" = api_key))
 
-  if(httr::status_code(resp) != 200){
-    status_httr <- httr::http_status(resp)
-    stop(paste("Category:", status_httr$category,
-               "Reason:", status_httr$reason,
-               "Message:", status_httr$message))
-  }
-
-  cont <- httr::content(resp, as = "text", encoding = "UTF-8")
-
-  cont_df <- jsonlite::fromJSON(cont)
-
-  if(length(cont_df) == 0){
+  if(nrow(cont_df) == 0){
     stop("No data with these parameters.")
   }
 
@@ -102,5 +92,112 @@ gie_lng_company_eic <- function(country_code, eic_code, eic_company_code, api_ke
     x
   })
   cont_df <- suppressMessages(readr::type_convert(cont_df, na = c("", "NA", "-")))
+  cont_df <- dplyr::arrange(cont_df, gasDayStart)
+
   cont_df
 }
+
+#' Get the historical data export for for a specific facility from a company within a country.
+#'
+#' @param country_code Two digit country code. Ex: NL, DE, DK, SE, FI etc.
+#' @param company_code The 21 digit eic code of the company as found on the API page.
+#' @param facility_code The 21 digit eic code of the facility as found on the API page.
+#' @param api_key The default is NULL and searches for your GIE_PAT in you .Renviron
+#'     file.
+#' @param max_pages Maximum pages. Defaults to 5000 to get all pages.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' library(gie)
+#'
+#' tail(gie_gas_facility_eic(country_code = "DE", company_code = "21X000000001160J"
+#'                           facility_code = "21Z000000000271O"))
+#'
+#'
+gie_gas_facility_eic <- function(country_code, company_code, facility_code, api_key = NULL, max_pages = 5000){
+
+  area <- toupper(country_code)
+
+  if(length(country_code) > 1){
+    stop("country_code only accepts a vector of length one.")
+  }
+
+
+  cont_df <- gie_pagination_api(hostname = "agsi.gie.eu", country = country_code,
+                                company = company_code,
+                                max_pages = max_pages, api_key = api_key)
+
+
+  if(nrow(cont_df) == 0){
+    stop("No data with these parameters.")
+  }
+
+  cont_df$info <- sapply(cont_df$info, function(x){
+    if(length(x) < 1){
+      x <- as.character(NA)
+    } else {
+      x <- paste(x, collapse = ";")
+    }
+    x
+  })
+  cont_df <- suppressMessages(readr::type_convert(cont_df, na = c("", "NA", "-")))
+  cont_df <- dplyr::arrange(cont_df, gasDayStart)
+
+  cont_df
+}
+
+#' Get the historical data export for for a specific facility from a company within a country.
+#'
+#' @param country_code Two digit country code. Ex: NL, DE, DK, SE, FI etc.
+#' @param company_code The 21 digit eic code of the company as found on the API page.
+#' @param facility_code The 21 digit eic code of the facility as found on the API page.
+#' @param api_key The default is NULL and searches for your GIE_PAT in you .Renviron
+#'     file.
+#' @param max_pages Maximum pages. Defaults to 5000 to get all pages.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' library(gie)
+#'
+#' eic <- gie_lng_facility_eic(country_code = "DE",
+#'                            company_code = "21X000000001368W"
+#'                            facility_code = "")
+#'
+#'
+gie_lng_facility_eic <- function(country_code, company_code, facility_code,
+                                 api_key = NULL, max_pages = 5000){
+
+  area <- toupper(country_code)
+
+  if(length(country_code) > 1){
+    stop("country_code only accepts a vector of length one.")
+  }
+
+
+  cont_df <- gie_pagination_api(hostname = "alsi.gie.eu", country = country_code,
+                                company = company_code, facility = facility_code,
+                                max_pages = max_pages, api_key = api_key)
+
+
+  if(nrow(cont_df) == 0){
+    stop("No data with these parameters.")
+  }
+
+  cont_df$info <- sapply(cont_df$info, function(x){
+    if(length(x) < 1){
+      x <- as.character(NA)
+    } else {
+      x <- paste(x, collapse = ";")
+    }
+    x
+  })
+  cont_df <- suppressMessages(readr::type_convert(cont_df, na = c("", "NA", "-")))
+  cont_df <- dplyr::arrange(cont_df, gasDayStart)
+
+  cont_df
+}
+
